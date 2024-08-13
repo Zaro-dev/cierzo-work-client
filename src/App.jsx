cf637decbb896328d90e9e6ec4b79865ff3b61c9
Mario <mariolazaroredolar@gmail.com>
2024-08-13 09:45:44 +0200

    cambios deploit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { Routes } from 'react-router-dom'
 import './App.css'
+import WelcomePage from './pages/WelcomePage'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div>
+      <Routes>
+      <Route path="/" element={<WelcomePage />} />
+      </Routes>
+    </div>
   )
 }
 
--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function WelcomePage() {
   return (
-    <div>WelcomePage</div>
+    <div>
+      <h1>Welcome Cierzo</h1>
+    </div>
   )
 }
 
